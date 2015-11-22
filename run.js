6df0bedd862989ede32e455c1da63f7bd2a8e6f2
Derrick <gengdeh@gmail.com>
2015-11-22 22:05:17 +0800

    return stdout

--- run.js
+++ run.js
@@ -13,10 +13,9 @@ app.use(cors());
 
 app.post('/', function (req, res) {
     var name = shortID.generate();
-    exec('docker run --name=' + name + ' -e "code=' + req.body.code + '" derrickh/codellege');
-    setTimeout(function () { exec('docker ps --filter "name=' + name + '" --format "{{.Ports}}"', function(err, stdout, stderr) {
+    exec('docker run --name=' + name + ' -e "code=' + req.body.code + '" derrickh/codellege', function (err, stdout, stderr) {
 	res.send(stdout);
-    })}, 1000);
+    });
     setTimeout(function() {
         exec('docker stop ' + name, function() {
 	    exec('docker rm ' + name);	
